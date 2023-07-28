7179633bb74d939de46825f2fd668e058b374c90
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2023-07-28 17:06:41 -0300

    chore: add database connection environment variables

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,10 @@
 const devEnv = {
   type: "postgres",
-  url: process.env.DATABASE_URL,
+  host: "localhost",
+  port: 5431,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
   entities: ["./src/entities/**/*.ts"],
   migrations: ["./src/database/migrations/*.ts"],
   cli: {
